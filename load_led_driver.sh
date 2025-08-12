4bd4ae57071679da886e929bcf443a0877c50c48
Tyler M <tmart234@gmail.com>
2025-08-12 17:26:55 -0600

    Update load_led_driver.sh

--- load_led_driver.sh
+++ load_led_driver.sh
@@ -3,15 +3,15 @@
 # This script is run at boot by the systemd service to load the custom
 # kernel module and configure the GPIO pin for the WS2812B LEDs.
 
-# Navigate to the kernel module directory, using the PI_USER variable
-cd /home/${PI_USER}/rpi_ws281x/rp1_ws281x_pwm
+# Use absolute paths to ensure commands work regardless of working directory
+DRIVER_DIR="/home/$PI_USER/rpi_ws281x/rp1_ws281x_pwm"
 
 # Unload any old module first to ensure a clean state.
 rmmod rp1_ws281x_pwm &> /dev/null || true
 
 # Load the new driver and configure the hardware
-insmod ./rp1_ws281x_pwm.ko
-dtoverlay -d . rp1_ws281x_pwm
+insmod $DRIVER_DIR/rp1_ws281x_pwm.ko
+dtoverlay -d $DRIVER_DIR $DRIVER_DIR/rp1_ws281x_pwm
 pinctrl set 18 a3 pn
 
 echo "WS281x LED driver loaded for Pi 5."
